e55b57ae7bf6a8340b94b2911d73528a3a3a2a8e
ICX\Palina.Rahila <palina.rahila@itechart-group.com>
2022-12-29 16:43:58 +0300

    get posts from API

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="wrapper">
-    <h2 class="title">All your posts in ine place</h2>
+  <h2 class="title">All your posts in ine place</h2>
+  <div class="wrapper" v-if="!isPostsLoading">
     <custom-button class="button" @click="openModal"
       >Create new post</custom-button
     >
     <PostList @delete="deletePost" :posts="posts" />
   </div>
+  <p v-else class="loading">Loading...</p>
   <custom-modal v-model:show="showModal">
     <PostForm @create="createPost" />
   </custom-modal>
@@ -14,6 +15,7 @@
 <script>
 import PostForm from "./components/PostForm.vue";
 import PostList from "./components/PostList.vue";
+import axios from "axios";
 
 export default {
   components: {
@@ -22,27 +24,9 @@ export default {
   },
   data() {
     return {
-      posts: [
-        {
-          id: 1,
-          title: "The Cheesecake Factory: Use humor and great photos",
-          description:
-            "The Cheesecake Factory is an American chain of restaurants, localized around the world. If you’re not familiar with it, you may recognize the name from the hit series The Big Bang Theory. They have a large following on Facebook and regularly post about food that’s on their menu.",
-        },
-        {
-          id: 2,
-          title: "Tony’s Chocolonely: Show people what’s happening",
-          description:
-            "Tony Chocolonely is a Dutch company that sells chocolate. Their chocolate bars are quite popular and their mission is to make chocolate 100% slave-free. As they are opinionated, which fits their mission, they often get a lot of engagement on their social posts. A year ago they opened a Chocolate Bar in Amsterdam.",
-        },
-        {
-          id: 3,
-          title: "LEGO: Involve your fans and let them get creative",
-          description:
-            "LEGO has lots of fans around the globe. And a product that people can easily get creative with. For Pride Month they asked people to create something that reflects pride. From all of the pictures they received, they chose a few highlights to share with everyone on Facebook.",
-        },
-      ],
+      posts: [],
       showModal: false,
+      isPostsLoading: false,
     };
   },
   methods: {
@@ -56,6 +40,28 @@ export default {
     openModal() {
       this.showModal = true;
     },
+    async getPosts() {
+      try {
+        this.isPostsLoading = true;
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts?_limit=10"
+        );
+        const newPosts = response.data.map((post) => {
+          return {
+            id: post.id,
+            title: post.title,
+            description: post.body,
+          };
+        });
+        this.posts = newPosts;
+        this.isPostsLoading = false;
+      } catch {
+        console.log("Error!");
+      }
+    },
+  },
+  mounted() {
+    this.getPosts();
   },
 };
 </script>
@@ -85,5 +91,12 @@ export default {
   color: darkmagenta;
   margin: 0 auto;
   margin-top: 10px;
+}
+.loading {
+  font-size: 25px;
+  text-align: center;
+  padding: 30px 0;
+  color: rgb(231, 135, 103);
+  font-weight: bold;
 }
 </style>
